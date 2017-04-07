83952726c0d17b9eaade240e5cb1fba95b0b0ef1
AnastasiaKhitryk <anastasia.khitryk@gmail.com>
2017-04-07 10:24:07 +0300

    Initial commit

---
+++ src/main/java/de/springboot/Application.java
@@ -0,0 +1,27 @@
+package de.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Анастасия on 07.04.2017.
+ */
+@Controller
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String showHelloWorld(){
+        return "hello-world";
+    }
+
+    @RequestMapping("/")
+    public String showMain(){
+        return "hello-world";
+    }
+}
